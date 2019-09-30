7e18a9dd01eb5e1b82c8288032cca2a2d631cfcf
Stefano Borini <stefano.borini@astrazeneca.com>
2019-09-30 14:29:59 +0100

    Added plot.interactive function

---
+++ R/plot.interactive.R
@@ -0,0 +1,22 @@
+plot.interactive <- function(tendril, opacity=0.5) {
+  cc= tendril$data[["p.adj"]]
+
+  cc.10 <- log10(cc)
+  cc.10[cc.10<(-3)] <- -3
+  tendrilpal <- colorRampPalette( c( "grey15", "red", "darkorange", "gold", "cornflowerblue"  ) )( 5 )
+  vals <- rescale(c(-3, -1.3, -1, -0.3, 0))
+
+  p <- tendril$data %>%
+    group_by(Terms) %>%
+    plot_ly(x=~x, y=~y, width = 700, height = 700,
+            mode = "lines+markers", type = "scatter",
+            marker = list(size=~TermsCount/100, opacity=opacity), color = ~cc.10,
+            colors = tendrilpal,
+            line = list(color = "lightgrey"),
+            text = ~paste("Term: ", Terms, '<br>Start day:', StartDay, '<br>p.adjusted:', round(p.adj, 4)),
+            hoverinfo = "text") %>%
+    layout(xaxis = list(nticks = 10, showticklabels = FALSE, title = ""),
+           yaxis = list(scaleanchor = "x", showticklabels = FALSE, title = "")
+    )
+  return(p)
+}
