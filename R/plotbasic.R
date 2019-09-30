46e35a957438b40c72974f285ff5dc526468dd71
Stefano Borini <stefano.borini@astrazeneca.com>
2019-09-30 14:26:00 +0100

    Added plotbasic routine

---
+++ R/plotbasic.R
@@ -0,0 +1,66 @@
+## Tendril plot, adjusted with darker border  ##
+plotbasic <- function(x, coloring = "Terms") {
+  plotdata=data.frame(x= x$data$x,
+                      y= x$data$y,
+                      Terms= x$data$Terms
+  )
+  cc= x$data[[coloring]]
+
+  data2.labels <- data.frame(
+    xpos = c(Inf, -Inf),
+    ypos = c(Inf, Inf),
+    label = x$Treatments,
+    hjust = c(1,0),
+    vjust = c(1,1)
+  )
+
+  y<-NULL
+  Terms <- NULL
+
+  if(coloring %in% c("p", "p.adj", "fish")) {
+    cc.10 <- log10(cc)
+    cc.10[cc.10<(-3)] <- -3
+    tendrilpal <- colorRampPalette( c( "grey15", "red", "darkorange", "gold", "cornflowerblue"  ) )( 5 )
+    vals <- rescale(c(-3, -1.3, -1, -0.3, 0))
+    p <- ggplot(data=x$data, aes(x=x, y=y, group=Terms, color=cc.10), aspect="iso") +
+      coord_fixed(ratio=1) +
+      labs(color = paste("10log", coloring)) +
+      geom_path(size=1) +
+      geom_point(aes(x=x, y=y, fill=cc.10, colour=cc.10, size=TermsCount), shape=21, stroke=0.1) +
+      scale_fill_gradientn(colours=tendrilpal, values = vals) +
+      scale_colour_gradientn(colours=darken(tendrilpal), values = vals) +
+      theme(axis.title.x = element_blank(),
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            legend.key.height=unit(2,"cm")) +
+      theme_bw() +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      annotate(geom="text", x=data2.labels$xpos, y=data2.labels$ypos,
+               label = data2.labels$label,
+               hjust=data2.labels$hjust, vjust=data2.labels$vjust,
+               colour = "white", size = 6, fontface = 2)
+  } else {
+    p <- ggplot(data=x$data, aes(x=x, y=y, group=Terms, color=cc), aspect="iso") +
+      coord_fixed(ratio=1) +
+      labs(color = coloring) +
+      geom_path(size=1) +
+      geom_point(size=3) +
+      theme(axis.title.x = element_blank(),
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks.y = element_blank()) +
+      theme_bw() +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      annotate(geom="text", x=data2.labels$xpos, y=data2.labels$ypos,
+               label = data2.labels$label,
+               hjust=data2.labels$hjust, vjust=data2.labels$vjust,
+               colour = "white", size = 6, fontface = 2)
+  }
+
+  return(p)
+}
