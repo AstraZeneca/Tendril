8cb5bf39c6d23f5d3634976438178b505e7f4e8a
Mark Edmondson-Jones <mark.edmondson-jones@astrazeneca.com>
2019-10-08 16:22:56 +0000

    Changed to allow multiple terms

--- R/ae_timeseries.R
+++ R/ae_timeseries.R
@@ -3,30 +3,34 @@ ae_timeseries <- function(tendril.data, term) {
   . <- plyr::.
   summarize <- plyr::summarize
 
-  time.serie <- plyr::ddply(tendril.data$data[tendril.data$data$Terms == term,], .(StartDay, Treat), summarize, n=length(Unique.Subject.Identifier))
-  time.s.2 <- reshape2::dcast(time.serie, StartDay ~ Treat, fun.aggregate = mean, value.var = "n")
+  time.serie <- plyr::ddply(tendril.data$data[tendril.data$data$Terms %in% term,], .(Terms, Treat, StartDay), summarize, n=length(Unique.Subject.Identifier))
+  time.s.2 <- reshape2::dcast(time.serie, Terms + StartDay ~ Treat, fun.aggregate = mean, value.var = "n")
 
   cum.na <- function(x) {
     x[which(is.na(x))] <- 0
     return(cumsum(x))
   }
 
-  time.s.2$treat1.cumsum <- cum.na(time.s.2[,2])
-  time.s.2$treat2.cumsum <- cum.na(time.s.2[,3])
-  time.s.2$Net <- time.s.2$treat1.cumsum-time.s.2$treat2.cumsum
+  time.grp <- dplyr::group_by(time.s.2, Terms)
+  time.grp <- dplyr::mutate_at(time.grp, 2:3, cum.na)
+  time.s.2 <- dplyr::ungroup(time.grp)
+  time.s.2 <- as.data.frame(time.s.2)
+  
+  time.s.2$Net <- time.s.2[,3]-time.s.2[,4]
+  time.s.2$Terms<-factor(time.s.2$Terms,levels=term)
   
   plot <- ggplot2::ggplot(
     data = time.s.2,
-    ggplot2::aes(x=StartDay, y=Net, col=)) +
-    ggplot2::ggtitle(term) +
+    ggplot2::aes(x=StartDay, y=Net, col=Terms)) +
+#    ggplot2::ggtitle(term) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::theme_bw() +
     ggplot2::xlab("Day since randomization") +
     ggplot2::ylab(paste("Net events - ",
-                        names(time.s.2)[2],
-                        " over ",
                         names(time.s.2)[3],
+                        " over ",
+                        names(time.s.2)[4],
                         sep = ""))
   return(plot)
 }
