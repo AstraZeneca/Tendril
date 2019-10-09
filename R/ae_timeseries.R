97e880074abcba1d34246687d57a11cf0323f1c9
Mark Edmondson-Jones <mark.edmondson-jones@astrazeneca.com>
2019-10-09 07:23:50 +0000

    Add functionality to plot one, selected or all terms.

--- R/ae_timeseries.R
+++ R/ae_timeseries.R
@@ -1,9 +1,17 @@
 ## Plot time series of net events on active arm ##
-ae_timeseries <- function(tendril.data, term) {
+ae_timeseries <- function(tendril.data, term=NULL) {
   . <- plyr::.
   summarize <- plyr::summarize
 
-  time.serie <- plyr::ddply(tendril.data$data[tendril.data$data$Terms %in% term,], .(Terms, Treat, StartDay), summarize, n=length(Unique.Subject.Identifier))
+  if (length(term)==0) {
+    time.serie <- plyr::ddply(tendril.data$data,
+                              .(Terms, Treat, StartDay),
+                              summarize, n = length(Unique.Subject.Identifier))
+  } else {
+    time.serie <- plyr::ddply(tendril.data$data[tendril.data$data$Terms %in% term,],
+                              .(Terms, Treat, StartDay),
+                              summarize, n = length(Unique.Subject.Identifier))
+  }
   time.s.2 <- reshape2::dcast(time.serie, Terms + StartDay ~ Treat, fun.aggregate = mean, value.var = "n")
 
   cum.na <- function(x) {
@@ -17,12 +25,19 @@ ae_timeseries <- function(tendril.data, term) {
   time.s.2 <- as.data.frame(time.s.2)
   
   time.s.2$Net <- time.s.2[,3]-time.s.2[,4]
-  time.s.2$Terms<-factor(time.s.2$Terms,levels=term)
-  
-  plot <- ggplot2::ggplot(
-    data = time.s.2,
-    ggplot2::aes(x=StartDay, y=Net, col=Terms)) +
-#    ggplot2::ggtitle(term) +
+  if (!is.null(term)) {
+    time.s.2$Terms<-factor(time.s.2$Terms,levels=term)
+  }
+
+  if (length(term)==1) {  
+    plot <- ggplot2::ggplot(data = time.s.2,
+                            ggplot2::aes(x=StartDay, y=Net)) +
+      ggplot2::ggtitle(term)
+  } else {
+    plot <- ggplot2::ggplot(data = time.s.2,
+                            ggplot2::aes(x=StartDay, y=Net, col=Terms))
+  }
+  plot <- plot +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::theme_bw() +
