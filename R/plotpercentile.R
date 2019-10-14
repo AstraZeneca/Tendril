4f13b0fe8976f2fca8cf4760eae56e8b2f4bfc97
Mark Edmondson-Jones <mark.edmondson-jones@astrazeneca.com>
2019-10-14 10:54:39 +0000

    Corrected colouring on percentile and permutation plots for Terms, rdiff, RR and OR

--- R/plotpercentile.R
+++ R/plotpercentile.R
@@ -1,4 +1,4 @@
-plotpercentile <- function(x){
+plotpercentile <- function(x, coloring){
 
   #prepare data
   dataset <- x$tendril$data
@@ -8,18 +8,20 @@ plotpercentile <- function(x){
   #retrieve data, permutations and percentile and bind in one dataframe
   data <- dataset[dataset$Terms == unique(perm.data$Terms),]
   actualdata <- createDataset(data$x, data$y, "Actual", "Actual", data$StartDay, perm.data)
-  actualdata$p.adj <- data$p.adj
+  actualdata$color <- data[[coloring]]
   normdf <- createDataset(0, 0, "Actual", "Actual", 0, perm.data)
-  normdf$p.adj <- 0
+  normdf$color <- 0
   actualdata<- rbind(normdf, actualdata)
   plotdata <- createDataset(perm.data$x, perm.data$y, perm.data$label, perm.data$type, perm.data$StartDay, perm.data)
-  plotdata$p.adj <- perm.data$p.adj
+  plotdata$color <- perm.data[[coloring]]
   plotdata<- rbind(actualdata, plotdata)
   percdata <- createDataset(percentile$x, percentile$y, percentile$label, percentile$type, percentile$StartDay, perm.data)
-  percdata$p.adj <- percentile$p.adj
+  percdata$color <- percentile[[coloring]]
   plotdata<- rbind(plotdata, percdata)
 
-  plotdata$cc.10<-pmax(log10(plotdata$p.adj), -3)
+  if (coloring %in% c("p","p.adj","fish")) {
+    plotdata$color<-pmax(log10(plotdata$color), -3)
+  }
   palette <- tendril_palette()
   
   #plot title
@@ -40,31 +42,46 @@ plotpercentile <- function(x){
     aes <- ggplot2::aes
     element_blank <- ggplot2::element_blank
 
-    p<- ggplot2::ggplot(data=plotdata[plotdata$type=="Permutation", ], aes(x=x, y=y, group=label), aspect="iso") +
-       ggplot2::scale_colour_gradientn("log(p-val)",
-                                       colours=palette$grpalette,
-                                       values = palette$values,
-                                       limits = palette$limits
-       ) +
-       ggplot2::geom_path(color="grey80") +
-       ggplot2::geom_path(data=plotdata[plotdata$type=="Percentile", ], aes(x=x, y=y, group=label), color="grey40", size=1.25)+
-       ggplot2::geom_path(data=plotdata[plotdata$type=="Actual", ], aes(x=x, y=y, group=label, color = cc.10))+
-       ggplot2::geom_point(data=plotdata[plotdata$type=="Actual", ], aes(x=x, y=y, group=label, color = cc.10), size=2) +
-       ggplot2::theme_bw() +
-       ggplot2::theme(axis.title.x = element_blank(),
+    p<- ggplot2::ggplot(data=plotdata[plotdata$type=="Permutation", ], aes(x=x, y=y, group=label), aspect="iso")
+    if (coloring %in% c("p","p.adj","fish")) {
+      p <- p +
+        ggplot2::scale_colour_gradientn("log(p-val)",
+                                        colours=palette$grpalette,
+                                        values = palette$values,
+                                        limits = palette$limits
+       )
+    } else if (coloring != "Terms") {
+      p <- p +
+        ggplot2::scale_colour_gradient(coloring)
+    }
+    p <- p +
+      ggplot2::geom_path(color="grey80") +
+      ggplot2::geom_path(data=plotdata[plotdata$type=="Percentile", ], aes(x=x, y=y, group=label), color="grey40", size=1.25)
+    if (coloring != "Terms") {
+      p <- p +
+        ggplot2::geom_path(data=plotdata[plotdata$type=="Actual", ], aes(x=x, y=y, group=label, color = color))+
+        ggplot2::geom_point(data=plotdata[plotdata$type=="Actual", ], aes(x=x, y=y, group=label, color = color), size=2)
+    } else {
+      p <- p +
+        ggplot2::geom_path(data=plotdata[plotdata$type=="Actual", ], aes(x=x, y=y, group=label), color = "blue")+
+        ggplot2::geom_point(data=plotdata[plotdata$type=="Actual", ], aes(x=x, y=y, group=label), color = "blue", size=2)
+    }
+    p <- p +
+      ggplot2::theme_bw() +
+      ggplot2::theme(axis.title.x = element_blank(),
             axis.text.x = element_blank(),
             axis.ticks.x = element_blank(),
             axis.title.y = element_blank(),
             axis.text.y = element_blank(),
             axis.ticks.y = element_blank()) +
-       ggplot2::theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-       ggplot2::labs(color = "Type") +
-       ggplot2::coord_fixed(ratio=1) +
-       ggplot2::ggtitle(Title) +
-       ggplot2::annotate(geom="text", x=data2.labels$xpos, y=data2.labels$ypos,
-               label = data2.labels$label,
-               hjust=data2.labels$hjust, vjust=data2.labels$vjust,
-               colour = "grey40", size = 5, fontface = 2)
+      ggplot2::theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      ggplot2::labs(color = "Type") +
+      ggplot2::coord_fixed(ratio=1) +
+      ggplot2::ggtitle(Title) +
+      ggplot2::annotate(geom="text", x=data2.labels$xpos, y=data2.labels$ypos,
+              label = data2.labels$label,
+              hjust=data2.labels$hjust, vjust=data2.labels$vjust,
+              colour = "grey40", size = 5, fontface = 2)
 
     return(p)
 }
