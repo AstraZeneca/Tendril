a20acaff7a0567d413edd424339c99a11b86914e
Stefano Borini <stefano.borini@astrazeneca.com>
2020-01-10 15:33:59 +0000

    Same palette for plotly plots

--- R/plotly_plotbasic.R
+++ R/plotly_plotbasic.R
@@ -1,46 +1,58 @@
-plotly_plotbasic <- function(tendril, coloring, term, opacity=0.5) {
+plotly_plotbasic <- function(tendril, coloring, term, opacity=1.0) {
   `%>%` <- magrittr::`%>%`
 
   plotdata <- tendril$data
   plotdata<-dplyr::arrange(plotdata, Terms, StartDay)
-  
+
   palette <- tendril_palette()
   max_termscount <- max(plotdata$TermsCount, na.rm = TRUE)
-  
+
   x.min <- min(plotdata$x)
   x.max <- max(plotdata$x)
   y.min <- min(plotdata$y)
   y.max <- max(plotdata$y)
-  
+
   if (length(term) > 0) {
     plotdata <- plotdata[plotdata$Terms %in% term,]
   }
-  
+
   if (!is.null(term)) {
     plotdata$Terms<-factor(plotdata$Terms,levels=term)
   }
-  
+
   if(coloring %in% c("p", "p.adj", "fish")) {
     plotdata$cc.10 <- pmax(log10(plotdata[[coloring]]), -3)
+    palette_data <- tendril_palette()
 
-    p <- plotdata %>% 
-      dplyr::group_by(Terms) %>% 
+    colorscale = list()
+    for (i in seq_len(length(palette_data$values))) {
+      colorscale[[i]] <- c(palette_data$values[[i]], palette_data$grpalette[[i]])
+    }
+
+    p <- plotdata %>%
+      dplyr::group_by(Terms) %>%
       plotly::plot_ly(x=~x, y=~y,type="scatter",mode="lines",
               line = list(color = "lightgrey"),
               showlegend=FALSE) %>%
       dplyr::ungroup() %>%
-      plotly::add_markers(x=~x, y=~y, 
-                  mode = "markers", type = "scatter", 
+      plotly::add_markers(x=~x, y=~y,
+                  mode = "markers", type = "scatter",
                   marker = list(size=~(TermsCount/max_termscount)*10,
                                 opacity=opacity,
-                                color = ~cc.10, 
-                                colorscale="RdBu",
-                                colorbar=list(title="log(p-val)")), 
+                                color = ~cc.10,
+                                colorscale=colorscale,
+                                zmin=palette_data[[1]],
+                                zmax=palette_data[[2]],
+                                line=list(
+                                  width = 0
+                                ),
+
+                                colorbar=list(title="log(p-val)")),
                   text = ~paste("Term: ", Terms,
                                 '<br>Start day:', StartDay,
                                 '<br>Frequency:', TermsCount,
                                 '<br>p.adjusted:', round(p.adj, 4),
-                                sep=""), 
+                                sep=""),
                   hoverinfo = "text",
                   inherit=FALSE) %>%
       plotly::add_annotations(
@@ -71,8 +83,8 @@ plotly_plotbasic <- function(tendril, coloring, term, opacity=0.5) {
                                   title = "")
       )
   } else {
-    p <- plotdata %>% 
-      dplyr::group_by(Terms) %>% 
+    p <- plotdata %>%
+      dplyr::group_by(Terms) %>%
       plotly::plot_ly(x=~x, y=~y,type="scatter",mode="lines+markers",
                       marker = list(size=~(TermsCount/max_termscount)*10,
                                     opacity=opacity),
@@ -82,7 +94,7 @@ plotly_plotbasic <- function(tendril, coloring, term, opacity=0.5) {
                                     '<br>Start day:', StartDay,
                                     '<br>Frequency:', TermsCount,
                                     '<br>p.adjusted:', round(p.adj, 4),
-                                    sep=""), 
+                                    sep=""),
                       hoverinfo = "text",
                       showlegend=TRUE) %>%
       plotly::add_annotations(
