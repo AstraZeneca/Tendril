a8cb8ef0b68656e236104e57f852cd17d9c5ce22
Stefano Borini <stefano.borini@astrazeneca.com>
2020-01-30 17:07:08 +0000

    Fix a problem with order found on development R

--- R/validation.R
+++ R/validation.R
@@ -1,9 +1,8 @@
 #------------------------
 # check if an input is a positive integer
 is.not.positive.integer <- function(x){
-  if (is.na(x) || !is.numeric(x) || x <= 0 || x %% 1 != 0 || length(x) != 1){
+  if (length(x) != 1 || is.na(x) || !is.numeric(x) || x <= 0 || x %% 1 != 0){
     res <- TRUE
-
   } else {
     res <- FALSE
   }
